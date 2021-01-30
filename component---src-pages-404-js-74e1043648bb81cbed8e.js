77b0b7a93f2fb21de6316f15aeb8ac157deeb5f7
Vladimir Zahradnik <vladimir@zahradnik.io>
2021-01-30 11:11:15 +0100

    Add generated page

---
+++ component---src-pages-404-js-74e1043648bb81cbed8e.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{w2l6:function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),l=t.n(a),r=t("7oih");n.default=function(){return l.a.createElement(r.a,null,l.a.createElement("div",null,l.a.createElement("h1",null,"STRÁNKA NENÁJDENÁ"),l.a.createElement("p",null,"Narazili ste na stránku, ktorá neexistuje...")))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-74e1043648bb81cbed8e.js.map
